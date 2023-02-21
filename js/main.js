ce04919a8a0ad0ede2974bf84945e0504ace2843
Giuseppe Suanno <suanno.giuseppe@yahoo.it>
2023-02-21 16:21:30 +0100

    Fine esercizio

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//Nome Utente
+let nome = prompt("inserisci il tuo nome");
+document.getElementById('nome').innerHTML;
+
+//Cognome utente
+
+let cognome = prompt("inserisci il tuo cognome");
+document.getElementById("cognome").innerHTML;
+
+
+//Colore preferito
+let colore = prompt("Il tuo colore preferito?");
+document.getElementById("colore").innerHTML;
+
+
+//tutto a schermo
+let mix;
+document.getElementById("mix").innerHTML = nome + cognome + colore + "23";
+
